bf42a0b0f59de4ac3c4380d980352503b80247f8
Shubham Kumar <Shubhamjnv97@gmail.com>
2024-01-13 20:59:06 +0530

    implemented the Sing out and update the profile and dispatch it

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,17 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValiData } from '../utils/validate';
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 const Login = () => {
   const [isSignInForm,setIsSignInForm] = useState(true);
   const [errorMessage,setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
@@ -24,7 +30,20 @@ const Login = () => {
       .then((userCredential) => {
       // Signed up 
       const user = userCredential.user;
+      updateProfile(user, {
+        displayName: name.current.value, photoURL: "https://example.com/jane-q-user/profile.jpg"
+      }).then(() => {
+        // Profile updated!
+        const {uid,email,displayName,photoURL} = auth.currentUser;
+        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
+        navigate("/browse");
+      }).catch((error) => {
+        // An error occurred
+        setErrorMessage(error.message)
+      });
+      
       console.log(user);
+
       // ...
       })
       .catch((error) => {
@@ -41,7 +60,9 @@ const Login = () => {
       .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
+
       console.log(user);
+      navigate("/browse");
       // ...
       })
       .catch((error) => {
